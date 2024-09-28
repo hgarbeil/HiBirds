f5e27e0bc9415142adbd600935934e0ab411fd9c
Harold Garbeil <hg@Harolds-MacBook-Pro.local>
2024-09-28 07:31:10 -1000

    Initial commit

---
+++ forest.js
@@ -0,0 +1,11 @@
+
+const main_container = document.querySelector(".main") ;
+
+
+function forestbirds () {
+
+    console.log("we are in here");
+    main_container.innerHTML = "What did we do?" ;
+
+
+}
