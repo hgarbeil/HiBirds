6ce558d63072f4bc8d1fd0282985b6c971a967d1
hg <hg@hgs-Mac-mini.local>
2024-10-05 12:18:06 -1000

    added birds

---
+++ seabirds.js
@@ -0,0 +1,38 @@
+const seabirdLink = document.getElementById("SeabirdLink") ;
+
+function seabirds () {
+    let infile='data/seabirds.txt';
+    arrlist=[] ;
+    for (ilink of linkarr){
+        let link = document.getElementById(ilink);
+        link.classList.remove ("active") ; 
+        
+    }
+    seabirdLink.classList.add("active") ;
+    arrlist=[] ;
+    let myhtml ="<div class='main-tile-head'><h2>Sea Birds</h2></div><div class='main-tile-container'>" ;
+    $ajaxUtils.sendGetRequest (infile, function(responseText){
+        lines = responseText.split('\n') ;
+        let inum = 0 ;
+        for (iline of lines) {
+            str = iline.split(';') ;
+            console.log(str.length) ;
+            if (str.length > 7){
+                bird={indval:inum, name:str[0],  commonName:str[1], scientific_name:str[2], 
+                endemic: str[3], habitat: str[5], concern: str[9], image:str[6], image1:str[7], image2:str[8] ,description:str[10]} ;
+            }
+            else 
+                continue ;    
+            
+           
+            myhtml = myhtml + makeCard(bird);
+            arrlist.push(bird) ;
+            inum++ ;
+        }
+        myhtml= myhtml+"</div>";
+        main_container.innerHTML = myhtml ;
+
+    }, false );
+
+
+}
